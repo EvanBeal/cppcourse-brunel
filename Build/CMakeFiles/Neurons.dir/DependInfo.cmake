0490d26fc9738e22ea8891269934cee49c8faa31
EvanBeal <32447627+EvanBeal@users.noreply.github.com>
2017-11-04 19:18:36 +0100

    Final Version

---
+++ Build/CMakeFiles/Neurons.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/evan/Desktop/NeuronMake/main.cpp" "/Users/evan/Desktop/NeuronMake/Build/CMakeFiles/Neurons.dir/main.cpp.o"
+  "/Users/evan/Desktop/NeuronMake/network.cpp" "/Users/evan/Desktop/NeuronMake/Build/CMakeFiles/Neurons.dir/network.cpp.o"
+  "/Users/evan/Desktop/NeuronMake/neuron.cpp" "/Users/evan/Desktop/NeuronMake/Build/CMakeFiles/Neurons.dir/neuron.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../googletest/include"
+  "../googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/evan/Desktop/NeuronMake
+# Build directory: /Users/evan/Desktop/NeuronMake/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(neuron_unittest "neuron_unittest")
+subdirs("googletest")
